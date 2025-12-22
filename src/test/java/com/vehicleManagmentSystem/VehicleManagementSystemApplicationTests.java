fe893455d27831037cbad944f7ae346daca7b838
bhoinikhil <bhoinikk@gmail.com>
2025-12-22 12:54:53 +0530

    create springboot project ticket no:VEHMS-M01-T001

---
+++ src/test/java/com/vehicleManagmentSystem/VehicleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
